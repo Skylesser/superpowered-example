4b9f6537d9a9ce3b29f1b5b5b42c9188c705b5db
Matthieu Regnauld <mregnauld@gmail.com>
2018-04-28 10:11:47 +0200

    update TODO

--- app/src/main/java/com/superpowered/superpoweredexample/MainActivity.java
+++ app/src/main/java/com/superpowered/superpoweredexample/MainActivity.java
@@ -20,7 +20,6 @@ public class MainActivity extends AppCompatActivity
 		// TODO : *-> si player.open() 2 fois de suite dans une séquence de téléchargement, player.fullyDownloadedFilePath redevient NULL
 		// TODO : *-> bug si pas assez de données : le son rejoue dès qu'il y a (beaucoup) moins de 2 secondes à jouer
 		// TODO : *-> bug aléatoire si coupure internet pendant le téléchargement d'un titre : la lecture se coupe, même s'il y a assez de données, et à la reconnextion, la lecture ne reprend pas
-		// TODO : tests transitions au tempo (changement de tempo en cours de route / enchaînements multiples)
 		
 		// TODO : télécharger depuis une position (voir open())
 		
